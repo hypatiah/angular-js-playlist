bcc16b1f1507e8c87319a697421648c60599468e
Danielle Hassid <dhassid@Danielles-MacBook-Pro.local>
2017-03-10 13:48:04 -0800

    Add controller funcitonality

--- app/app.js
+++ app/app.js
@@ -1,17 +1,25 @@
 // modules
 var myNinjaryApp = angular.module('myNinjaApp', []);
 
-//function that fires before application runs, i.e. routing
-myNinjaryApp.config(function(){
+// //function that fires before application runs, i.e. routing
+// myNinjaryApp.config(function(){
+//
+// });
+//
+// // function that fires when applicaiton runs
+// myNinjaryApp.run(function(){
+//
+// });
+//
+// //create a controller
+// myNinjaryApp.controller();
+//
+// //all of this above^ resides wihtin the myNinjaryApp module
 
-});
+// use ['$scope', function($scope){ }] instead of just function($scope) to allow functionality after minifying
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 
-// function that fires when applicaiton runs
-myNinjaryApp.run(function(){
+  $scope.message = "hey yu"
 
-});
-
-//create a controller
-myNinjaryApp.controller();
-
-//all of this above^ resides wihtin the myNinjaryApp module
+  $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun']
+}]);
