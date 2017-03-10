dfbc53c681ef013d36f91fdd62b75bc9d2cb5e22
Danielle Hassid <dhassid@Danielles-MacBook-Pro.local>
2017-03-10 13:28:25 -0800

    Create module

---
+++ app/app.js
@@ -0,0 +1,17 @@
+// modules
+var myNinjaryApp = angular.module('myNinjaApp', []);
+
+//function that fires before application runs, i.e. routing
+myNinjaryApp.config(function(){
+
+});
+
+// function that fires when applicaiton runs
+myNinjaryApp.run(function(){
+
+});
+
+//create a controller
+myNinjaryApp.controller();
+
+//all of this above^ resides wihtin the myNinjaryApp module
